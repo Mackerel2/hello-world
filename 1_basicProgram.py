58e537e0de1811b41bada79fe0af2befae54d0f7
Mackerel2 <77627086+Mackerel2@users.noreply.github.com>
2021-01-25 15:49:51 -0400

    Add files via upload

---
+++ 1_basicProgram.py
@@ -0,0 +1,12 @@
+
+def program_1():
+    a=2
+    f = 2e4
+    
+    print(a)
+    print("is an integer")
+    print(f)
+    print("is a floating point number")
+    
+if __name__== '__main__':
+        program_1()
